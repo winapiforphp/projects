82bc217e8fecc8ab6ddcf9bbea316e46a6fcdca2
Elizabeth M Smith <auroraeosrose@gmail.com>
2012-09-09 20:43:15 -0400

    Manifest and changes for making gui build
    still lots of work to do here

---
+++ gui/run-tests.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+SET CURRENT=%~dp0
+
+SET TEST_PHP_EXECUTABLE=%CURRENT%\..\binaries\x86\Debug_TS\php.exe
+
+call %TEST_PHP_EXECUTABLE% "%CURRENT%\..\binaries\run-tests.php" -d extension_dir="%CURRENT%\..\Debug_TS" -d extension=php_winsystem.dll -d extension=php_wingdi.dll "%CURRENT%\..\..\gdi\tests"
+
+pause
