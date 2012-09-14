4bc2701eb810f08c74e03c3adfafb848586172ec
Elizabeth M Smith <auroraeosrose@gmail.com>
2012-09-14 15:06:31 -0400

    fixed to gui build
    Made gui build correctly, removed files for services stuff (now in it's
    own repo

--- gui/run-tests.bat
+++ gui/run-tests.bat
@@ -3,6 +3,6 @@ SET CURRENT=%~dp0
 
 SET TEST_PHP_EXECUTABLE=%CURRENT%\..\binaries\x86\Debug_TS\php.exe
 
-call %TEST_PHP_EXECUTABLE% "%CURRENT%\..\binaries\run-tests.php" -d extension_dir="%CURRENT%\..\Debug_TS" -d extension=php_winsystem.dll -d extension=php_wingdi.dll "%CURRENT%\..\..\gdi\tests"
+call %TEST_PHP_EXECUTABLE% "%CURRENT%\..\binaries\run-tests.php" "%CURRENT%\..\..\gui\tests\Inputing"
 
 pause
